ef792054e30c8116de3537c09ebbdff29402327b
AlexGT <galext@gmail.com>
2018-04-13 03:06:07 +0300

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,9 @@
 class PostsController < ApplicationController
 
-<<<<<<< HEAD
 def new
   @post = Post.new
 end
  
-=======
-def new	
-	@post = Post.new
-end
-
->>>>>>> c4932cfc7bd3931a33f47ac621312c3d551772c2
 def create
   @post = Post.new(params[:post])
  
@@ -22,7 +15,6 @@ def create
 end
 
 def show
-<<<<<<< HEAD
   @post = Post.find(params[:id])
 end
 
@@ -32,21 +24,6 @@ end
 
 def update
   @post = Post.find(params[:id])
-=======
-@post = Post.find(params[:id])
-end
-
-def index
-	@posts = Post.all 
-end
-
-def edit
-  @post = Post.find(params[:id])
-end
-
-def update
-	@post = Post.find(params[:id])
->>>>>>> c4932cfc7bd3931a33f47ac621312c3d551772c2
  
   if @post.update_attributes(params[:post])
     redirect_to :action => :show, :id => @post.id
@@ -55,18 +32,14 @@ def update
   end
 end
 
-<<<<<<< HEAD
 def edit
-	@post = Post.find(params[:id])
+  @post = Post.find(params[:id])
 end
 
 def destroy
   @post = Post.find(params[:id])
   @post.destroy
- 
   redirect_to :action => :index
 end
 
-=======
->>>>>>> c4932cfc7bd3931a33f47ac621312c3d551772c2
 end
